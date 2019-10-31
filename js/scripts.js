627b314da602bbff0b5b03670703a204484bd35d
Student <example@example.com>
2019-10-31 14:38:29 -0700

    get input from user and display roman numeral

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var testArray = ["3","4","1", "0"]
+// var testArray = ["3","4","1", "0"]
 var oneToNine = ["1","2","3","4", "5", "6", "7", "8", "9"]
 var romanOnes = ['i', 'ii', 'iii', 'iv', 'v', 'vi', 'vii', 'viii', 'ix', 'x']
 var romanTens = ['x', 'xx', 'xxx', 'xl', 'l', 'lx', 'lxx', 'lxxx', 'xc']
@@ -10,16 +10,16 @@ var userArray = []
 
 var thousandsfunction = function(thousand){
   for (i = 0; i < oneToNine.length; i++){
-    if (testArray.length === 4 && oneToNine[i] == testArray[0] && testArray[0] < 4){
+    if (thousand.length === 4 && oneToNine[i] == thousand[0] && thousand[0] < 4){
       userArray.push(romanThousands[i])
       for (i = 0; i < oneToNine.length; i++){
-        if (testArray.length === 4 && oneToNine[i] == testArray[1]){
+        if (thousand.length === 4 && oneToNine[i] == thousand[1]){
           userArray.push(romanHundreds[i])
           for (i = 0; i < oneToNine.length; i++){
-            if (testArray.length === 4 && oneToNine[i] == testArray[2]){
+            if (thousand.length === 4 && oneToNine[i] == thousand[2]){
               userArray.push(romanTens[i])
               for (i = 0; i < oneToNine.length; i++){
-                if (testArray.length === 4 && oneToNine[i] == testArray[3]){
+                if (thousand.length === 4 && oneToNine[i] == thousand[3]){
                   userArray.push(romanOnes[i])
                 }
               }
@@ -32,93 +32,60 @@ var thousandsfunction = function(thousand){
 }
 var hundredsfunction = function(hundred){
   for (i = 0; i < oneToNine.length; i++){
-    if (testArray.length === 3 && oneToNine[i] == testArray[0]){
+    if (hundred.length === 3 && oneToNine[i] == hundred[0]){
       userArray.push(romanHundreds[i])
       for (i = 0; i < oneToNine.length; i++){
-        if (testArray.length === 3 && oneToNine[i] == testArray[1]){
+        if (hundred.length === 3 && oneToNine[i] == hundred[1]){
           userArray.push(romanTens[i])
           for (i = 0; i < oneToNine.length; i++){
-            if (testArray.length === 3 && oneToNine[i] == testArray[2]){
+            if (hundred.length === 3 && oneToNine[i] == hundred[2]){
               userArray.push(romanOnes[i])
             }
           }
         }
       }
-      console.log(userArray)
     }
   }
 }
+var tensfunction = function(ten){
+  for (i = 0; i < oneToNine.length; i++){
+    if (ten.length === 2 && oneToNine[i] == ten[0]){
+      userArray.push(romanTens[i])
+      for (i = 0; i < oneToNine.length; i++){
+        if (ten.length === 2 && oneToNine[i] == ten[1]){
+          userArray.push(romanOnes[i])
+        }
+      }
+    }
+  }
+}
+var onesfunction = function(one){
+  for (i = 0; i < oneToNine.length; i++){
+    if (one.length === 1 && oneToNine[i] == one[0]){
+      userArray.push(romanOnes[i])
+    }
+  }
+}
+console.log(userArray)
 
 
 
 
-//
-// var hundredsFunction = function(hundred){
-  //   for (i = 0; i < oneToNine.length; i++){
-    //     if (testArray.length === 3 && oneToNine[i] == testArray[1]){
-      //       userArray.push(romanHundreds[i])
-      //       for (i = 0; i < oneToNine.length; i++){
-        //         if (testArray.length === 3 && oneToNine[i] == testArray[2]){
-          //           userArray.push(romanTens[i])
-          //           for (i = 0; i < oneToNine.length; i++){
-            //             if (testArray.length === 3 && oneToNine[i] == testArray[3]){
-              //               userArray.push(romanOnes[i])
-              //               // console.log(userArray)
-              //             }
-              //           }
-              //         }
-              //       }
-              //     }
-              //   }
-              // } else if (testArray.length === 3 && oneToNine[i] == testArray[0] && testArray[0] < 3){
-                //   userArray.push(romanHundreds[i])
-                //   for (i = 0; i < oneToNine.length; i++){
-                  //     if (testArray.length === 3 && oneToNine[i] == testArray[1]){
-                    //       userArray.push(romanTens[i])
-                    //       for (i = 0; i < oneToNine.length; i++){
-                      //         if (testArray.length === 3 && oneToNine[i] == testArray[2]){
-                        //           userArray.push(romanOnes[i])
-                        //         }
-                        //       }
-                        //     }
-                        //   }
-                        // } else if (testArray.length === 2 && oneToNine[i] == testArray[0] && testArray[0] < 2){
-                          //   userArray.push(romanTens[i])
-                          //   for (i = 0; i < oneToNine.length; i++){
-                            //     if (testArray.length === 2 && oneToNine[i] == testArray[1]){
-                              //       userArray.push(romanOnes[i])
-                              //     }
-                              //   }
-                              // } else if (testArray.length === 1 && oneToNine[i] == testArray[0] && testArray[0] < 1){
-                                //   for (i = 0; i < oneToNine.length; i++){
-                                  //     if (testArray.length === 1 && oneToNine[i] == testArray[1]){
-                                    //       userArray.push(romanOnes[i])
-
-                                    // var tensfunction = function(tens){
-                                      //   for (i = 0; i < oneToNine.length; i++){
-                                        //     if (testArray.length === 2 && oneToNine[i] == testArray[0]){
-                                          //       userArray.push(romanTens[i])
-                                          //       for (i = 0; i < oneToNine.length; i++){
-                                            //         if (testArray.length === 2 && oneToNine[i] == testArray[1]){
-                                              //           userArray.push(romanOnes[i])
-                                              //           console.log(userArray)
-                                              //         }
-                                              //       }
-                                              //     }
-                                              //   }
-                                              // }
-                                              //
-
-
-
-
-                                              //User Interface
-                                              $(document).ready(function(){
-                                                $(".userInput").submit(function(event){
-                                                  event.preventDefault();
-                                                  var userInput = $("#userNumber").val();
-                                                  var userNumbers = userInput.split('');
-
-                                                  $("#userResult").text(userInput);
-                                                });
-                                              });
+//User Interface
+$(document).ready(function(){
+  $(".userInput").submit(function(event){
+    event.preventDefault();
+    var userInput = $("#userNumber").val();
+    var testArray = userInput.split('');
+    if (testArray.length === 4) {
+      thousandsfunction(testArray);
+    } else if (testArray.length === 3) {
+      hundredsfunction(testArray);
+    } else if (testArray.length === 2) {
+      tensfunction(testArray);
+    } else if (testArray.length === 1) {
+      onesfunction(testArray);
+    };
+    $("#userResult").text(userArray.join('').toUpperCase());
+  });
+});
